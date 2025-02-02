4916c06d1aa893eb8e69a5b87c4490171fada87c
Patrick Schaffner <patrick.schaffner@protonmail.com>
2025-02-02 16:41:20 +0100

    Implemented qualifiers.

--- pydi/inject.py
+++ pydi/inject.py
@@ -1,7 +1,42 @@
 from inspect import Parameter, signature
-from typing import Callable, Dict, Tuple, Any, get_args
+from typing import Callable, Dict, Tuple, Any, get_args, Annotated
 
 from makefun import wraps
+
+
+class ComponentDescriptor(object):
+
+    def __init__(self, target: type, qualifiers: dict[str, str]):
+        self._target: type = target
+        self._qualifiers: dict[str, str] = qualifiers
+
+    @property
+    def target(self):
+        return self._target
+
+    @property
+    def qualifiers(self):
+        return self._qualifiers
+
+    def __hash__(self):
+        h = 103 + hash(self.target)
+        for key, value in sorted(self.qualifiers.items()):
+            h = h ^ hash(key) ^ hash(value)
+        return h
+
+    def __eq__(self, other):
+        if not isinstance(other, ComponentDescriptor):
+            return False
+        if self.target != other.target:
+            return False
+        s = self.qualifiers
+        o = other.qualifiers
+        return len(s) == len(o) \
+            and set(s.keys()) == set(o.keys()) \
+            and all(s_i == o_i for s_i, o_i in zip(sorted(s.items()), sorted(o.items())))
+
+    def __str__(self):
+        return f"{self.target:str}:{compile_qualifiers(self.qualifiers)}"
 
 
 class Context:
@@ -11,22 +46,26 @@ class Context:
 
     def register(self,
                  target: type,
-                 component: object,
+                 factory: object,
+                 qualifiers: dict[str, str] = dict(),
                  ):
-        if target in self._singletons:
-            raise ValueError(f"Cannot register multiple components for '{target}'")
-        self._singletons[target] = component
+        descriptor = ComponentDescriptor(target, qualifiers)
+        if descriptor in self._singletons:
+            raise ValueError(f"Cannot register multiple components for '{descriptor}'")
+        self._singletons[descriptor] = factory
 
     def resolve(self,
                 target: type,
+                qualifiers: dict[str, str] = dict(),
+                *,
                 all: bool = False,
-                named: bool = False,
                 ):
-        if all or named:
+        if all:
             raise NotImplementedError("Not implemented yet.")
-        if target in self._singletons:
-            return self._singletons[target]()
-        raise ValueError(f"Cannot resolve dependency '{target}'.")
+        descriptor = ComponentDescriptor(target, qualifiers)
+        if descriptor in self._singletons:
+            return self._singletons[descriptor]()
+        raise ValueError(f"Cannot resolve dependency '{descriptor}'.")
 
 
 ctx = Context()
@@ -49,7 +88,7 @@ def provides(target: type | None = None, *, function: bool = False, **qualifiers
             if target is None:
                 target = signature(func).return_annotation
             factory = func
-        ctx.register(target, factory)
+        ctx.register(target, factory, qualifiers)
         return func
     return _decorator
 
@@ -69,7 +108,15 @@ def singleton():
     return _decorator
 
 
-def inject():
+def inject(*types, **qualifiers):
+    if len(types) > 1:
+        raise ValueError("Cannot inject more than one type per parameter.")
+    if len(types) == 1:
+        return Annotated[types[0], inject, compile_qualifiers(qualifiers)]
+
+    if len(qualifiers) > 0:
+        raise ValueError('Cannot specify qualifiers for inject decorator.')
+
     def _decorator(target):
         injector = Injector(target)
 
@@ -83,11 +130,39 @@ def inject():
     return _decorator
 
 
+QUALIFIERS_PREFIX = 'qualifiers:'
+
+
+def qualifiers(**q: str):
+    return compile_qualifiers(q)
+
+
+def compile_qualifiers(qualifiers: dict[str, str]):
+    qualifiers = [f'{key}={value}' for (key, value) in sorted(qualifiers.items())]
+    qualifiers = f"{QUALIFIERS_PREFIX}{','.join(qualifiers)}"
+    return qualifiers
+
+
+def parse_qualifiers(qualifiers: str):
+    if not qualifiers.startswith(QUALIFIERS_PREFIX):
+        raise ValueError(f"Qualifier string does not start with '{QUALIFIERS_PREFIX}'")
+    qualifiers = [tuple(q.split('=')) for q in qualifiers[len(QUALIFIERS_PREFIX):].split(',')]
+    qualifiers = dict(qualifiers)
+    return qualifiers
+
+
 class Injector:
+
+    @classmethod
+    def _parse_inject(cls, parameter: Parameter) -> tuple[type, dict[str, str]]:
+        args = get_args(parameter.annotation)
+        t = args[0]
+        q = parse_qualifiers(args[2]) if len(args) >= 3 else dict()
+        return t, q
 
     def __init__(self, injection_point: Callable, /):
         self._parameters = [p for p in signature(injection_point).parameters.values()]
-        self._injects = {p: get_args(p.annotation)[0] for p in self._parameters
+        self._injects = {p: self._parse_inject(p) for p in self._parameters
                          if hasattr(p.annotation, '__metadata__') and inject in p.annotation.__metadata__}
 
     @property
@@ -112,7 +187,7 @@ class Injector:
         while _param_kind_is(Parameter.POSITIONAL_ONLY):
             param = self._parameters[param_idx]
             if param in self._injects:
-                merged_args.append(context.resolve(self._injects[param]))
+                merged_args.append(context.resolve(*self._injects[param]))
             else:
                 if arg_idx >= len(args):
                     raise ValueError("Not enough positional arguments.")
@@ -124,7 +199,7 @@ class Injector:
         while _param_kind_is(Parameter.POSITIONAL_OR_KEYWORD):
             param = self._parameters[param_idx]
             if param in self._injects:
-                merged_args.append(context.resolve(self._injects[param]))
+                merged_args.append(context.resolve(*self._injects[param]))
             elif arg_idx >= len(args):
                 break
             else:
@@ -137,7 +212,7 @@ class Injector:
         # Fill variable-length positional parameters.
         while _param_kind_is(Parameter.VAR_POSITIONAL):
             if param in self._injects:
-                merged_args.extend(context.resolve(self._injects[param], all=True))
+                merged_args.extend(context.resolve(*self._injects[param], all=True))
             else:
                 remaining = args[arg_idx:]
                 merged_args.extend(remaining)
@@ -150,7 +225,7 @@ class Injector:
             if param in self._injects:
                 if param.name in kwargs:
                     raise ValueError("Cannot provide argument twice.")
-                merged_kwargs[param.name] = context.resolve(self._injects[param])
+                merged_kwargs[param.name] = context.resolve(*self._injects[param])
             elif param.name in kwargs:
                 merged_kwargs[param.name] = kwargs[param.name]
                 del kwargs[param.name]
@@ -161,7 +236,7 @@ class Injector:
         # Fill variable-length keyword parameters.
         while _param_kind_is(Parameter.VAR_KEYWORD):
             if param in self._injects:
-                merged_kwargs.update(context.resolve(self._injects[param], all=True, named=True))
+                merged_kwargs.update(context.resolve(*self._injects[param], all=True, named=True))
             else:
                 for key in list(kwargs.keys()):
                     merged_kwargs[key] = kwargs[key]
--- run_example.py
+++ run_example.py
@@ -1,34 +1,40 @@
 from typing import Annotated, Callable
-from pydi.inject import inject, singleton, provides
+from pydi.inject import inject, singleton, provides, qualifiers
 
 
-@provides()
+@provides(name='y')
 @singleton()
 def get_y() -> float:
     return 5
 
 
-@provides()
+@provides(name='x')
 def get_x() -> int:
     return 10
 
 
 @provides(function=True)
 @inject()
-def func(x: Annotated[int, inject],
-         z: int,
+def func(x: Annotated[int, inject, qualifiers(name='x')],
          /,
+         z: int,
          w: float = 0,
          *,
-         y: Annotated[float, inject],
+         y: inject(float, name='y'),
          v: float,
          ) -> float:
     return x * y * z * w * v
 
 
+@provides(name='f')
 @inject()
-def main(f: Annotated[Callable[[int, float, float], float], inject]):
-    print(f(1, w=1, v=1))
+def calculate(f: Annotated[Callable[[int, float, float], float], inject]) -> float:
+    return f(1, w=1, v=1)
+
+
+@inject()
+def main(f: inject(float, name='f')):
+    print(f)
 
 
 if __name__ == '__main__':
