ce9becebe29b882268cc038ebc2ff3872c6247d9
Patrick Schaffner <patrick.schaffner@protonmail.com>
2025-02-12 00:50:51 +0100

    Added method support for provides decorator.

--- pydi/container.py
+++ pydi/container.py
@@ -1,5 +1,5 @@
 from typing import Callable, Annotated, TypeVar, Any
-from inspect import signature
+import inspect
 
 from makefun import wraps
 
@@ -34,25 +34,59 @@ class Container(InjectionContext):
             target = None
         qualifiers = Qualifiers.for_provider(*flags, **params)
 
-        def _decorator(func: Callable[[], T]):
-            nonlocal target
-            provider: Callable[[], T]
-            if function:
+        def _register_provider(t: type[T], q: Qualifiers, p: Callable[[], T]) -> None:
+            c = Component[T](t, q)
+            self.registry.register(c, p)
+
+        if function:
+            def _func_decorator(func: Callable[[Any,...], T]) -> Callable[[Any,...], T]:
+                nonlocal target
                 if target is None:
-                    sig = signature(func)
+                    sig = inspect.signature(func)
                     return_type = sig.return_annotation
                     param_types = [p.annotation for p in sig.parameters.values()]
                     target = Callable[[*param_types], return_type]
-                provider = lambda: func
-            else:
-                if target is None:
-                    target = signature(func).return_annotation
-                provider = func
-            component = Component[T](target, qualifiers)
-            self.registry.register(component, provider)
-            return func
+                _register_provider(target, qualifiers, lambda: func)
+                return func
+            return _func_decorator
 
-        return _decorator
+        def _provider_decorator(provider: Callable[[], T]):
+            nonlocal target
+            if target is None:
+                target = provider.__annotations__['return']
+            _register_provider(target, qualifiers, provider)
+            return provider
+
+        def _register_method(owner: type, provider: Callable[[], T]) -> None:
+            nonlocal target
+            if target is None:
+                target = provider.__annotations__['return']
+            inj = self.inject
+
+            @inj()
+            def _method_provider(instance: inj(owner)) -> target:
+                return provider(instance)
+            _register_provider(target, qualifiers, _method_provider)
+
+        class _ProviderDescriptor:
+
+            def __init__(self, provider: Callable[[], T]):
+                self._provider = provider
+
+                n_params: int = len(inspect.signature(provider).parameters)
+                if n_params == 0:  # Function or staticmethod
+                    _provider_decorator(provider)
+                elif n_params > 1:  # Not a provider function
+                    raise ValueError(f'Provider {provider} has unfilled parameters.')
+
+            def __set_name__(self, owner, name):
+                setattr(owner, name, self._provider)
+                _register_method(owner, self._provider)
+
+            def __call__(self, *args, **kwargs):
+                return self._provider(*args, **kwargs)
+
+        return _ProviderDescriptor
 
     def inject(self, target: type[T] | None = None, *qualifiers, **kw_qualifiers):
         if target is not None and isinstance(target, str):
@@ -95,11 +129,12 @@ class Container(InjectionContext):
             raise ValueError("Parameter 'named=True' can only be used with 'many=True'.")
         instance = None
         origin = None
+        usdexc = None
         try:
             instance = self.registry.resolve(request, many=False, named=False, constraint=constraint)
             origin = self
-        except UnsatisfiedDependencyException:
-            pass
+        except UnsatisfiedDependencyException as e:
+            usdexc = e
         for (container, container_constraints) in self._dependencies.items():
             try:
                 instance = container.registry.resolve(request, many=False, named=False,
@@ -107,10 +142,10 @@ class Container(InjectionContext):
                 if origin is not None:
                     raise AmbiguousDependencyException(f"Ambiguous dependency {request} received from containers {origin.name} and {container.name}.")
                 origin = container
-            except UnsatisfiedDependencyException:
-                pass
+            except UnsatisfiedDependencyException as e:
+                usdexc = e
         if instance is None:
-            raise UnsatisfiedDependencyException(f"Cannot resolve dependency {request}")
+            raise UnsatisfiedDependencyException(f"Cannot resolve dependency {request}") from usdexc
         return instance
 
     def expose_to(self, other: 'Container', target: type[T], *tags: str, **params: str) -> None:
---
+++ run_object_example.py
@@ -0,0 +1,47 @@
+from pydi import Container, qualifiers as q, Inject
+
+container = Container(__name__)
+provides = container.provides
+inject = container.inject
+
+
+@provides()
+def get_x() -> int:
+    return -2
+
+
+class A:
+
+    @inject()
+    def __init__(self, base: Inject[float]):
+        self._base = base
+
+    @inject()
+    def do_a(self, x: Inject[int]) -> float:
+        return self._base*x
+
+    @staticmethod
+    @provides()
+    def get_base() -> float:
+        return 5.0
+
+
+class B:
+
+    @provides()
+    def create_A(self) -> A:
+        return A()
+
+
+@provides()
+def create_B() -> B:
+    return B()
+
+
+@inject()
+def main(a: Inject[A]) -> None:
+    print(a.do_a())
+
+
+if __name__ == '__main__':
+    main()
